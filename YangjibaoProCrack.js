81c6a5cbbd8385c21a50ef67254c33b339d56469
n0back <77069266+n0back@users.noreply.github.com>
2026-01-16 16:30:35 +0800

    Update YangjibaoProCrack.js

--- YangjibaoProCrack.js
+++ YangjibaoProCrack.js
@@ -17,7 +17,7 @@
 [rewrite_local]
   
 # > 养基宝☆解锁会员权限（2023-12-30）@ddgksf2013
-^https?:\/\/.*yangjibao\.com\/(wxapi\/)?account url script-response-body https://github.com/n0back/Scripts/raw/main/YangJiBaoProCrack.js
+^https?:\/\/.*yangjibao\.com\/(wxapi\/)?account url script-response-body https://raw.githubusercontent.com/n0back/Scripts/refs/heads/main/YangjibaoProCrack.js
 
 
 [mitm] 
